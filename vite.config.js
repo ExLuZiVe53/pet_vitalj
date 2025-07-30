95d154a26adb26b56773f560cb7d02de28325b12
ExLuZiVe53 <imstudent27@gmail.com>
2025-07-30 17:08:58 +0200

    added header css

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
+import createSvgSpritePlugin from "vite-plugin-svg-sprite";
 
 export default defineConfig({
-  plugins: [injectHTML()],
+  plugins: [
+    createSvgSpritePlugin({
+      exportType: "vanilla", // or 'react' or 'vue'
+      include: "**/icons/*.svg",
+    }),
+    [injectHTML(), FullReload(["./src/**/**.html"])],
+  ],
 });
